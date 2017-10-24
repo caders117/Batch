bea5dbeb42eb2b8deea0255b4f51ef258acd8688
caders117 <31219010+caders117@users.noreply.github.com>
2017-10-24 12:10:26 -0700

    Update BatchTest.bat

--- BatchTest.bat
+++ BatchTest.bat
@@ -1,12 +1,33 @@
 @echo off
+REM BatchTest.bat
+REM Playing around with Batch
+
+color 07
 set /p name="What is your name? "
 echo Hi %name%
+:start
 set /p ans="What would you like to do? (A)dd or (S)ubtract: "
 echo %ans%
 if /i %ans%==a (goto add) else if %ans%==s (goto subtract)
 :back
-echo testtest
-pause
+set /p ans="Play again? (Y/N): "
+if /i %ans%==y (goto start)
+color 02
+echo BBBBBBBBB         AAA       TTTTTTTTTT   CCCCCCCC   HH        HH
+echo BB      BB       AA AA          TT      CC       C  HH        HH
+echo BB      BB      AA   AA         TT      CC          HH        HH
+echo BB      BB     AA     AA        TT      CC          HH        HH
+echo BB      BB    AA       AA       TT      CC          HHHHHHHHHHHH
+echo BBBBBBBBB     AA       AA       TT      CC          HHHHHHHHHHHH
+echo BB      BB    AAAAAAAAAAA       TT      CC          HH        HH
+echo BB      BB   AA         AA      TT      CC          HH        HH
+echo BB      BB   AA         AA      TT      CC          HH        HH
+echo BB      BB   AA         AA      TT      CC       C  HH        HH
+echo BBBBBBBBB   AA           AA     TT       CCCCCCCC   HH        HH
+
+echo Thank you goodbye.
+echo Press any key to quit
+pause >nul
 exit /b
 
 :add
